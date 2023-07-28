172a4a8f7928dbe6b4ecd32ff21f311fa1fe8672
Nabina Mallik <109237169+nabinamallik@users.noreply.github.com>
2023-07-28 23:55:00 +0530

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-
+function getCurrentLocation() {
+    if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+                fetchWeatherData(latitude, longitude);
+            },
+            error => console.error('Error getting location:', error)
+        );
+    } else {
+        console.error('Geolocation is not supported by this browser.');
+    }
+}
 function fetchWeatherData(latitude, longitude) {
     const apiKey = '3534f601a2c63c2fe4f04f01f6e41361';
     const apiUrl = `http://api.weatherstack.com/current?access_key=${apiKey}&query=${latitude},${longitude}`;
@@ -16,30 +29,4 @@ function fetchWeatherData(latitude, longitude) {
         })
         .catch(error => console.error('Error fetching weather data:', error));
 }
-function getCurrentLocation() {
-    if ('geolocation' in navigator) {
-        navigator.geolocation.getCurrentPosition(
-            position => {
-                const latitude = position.coords.latitude;
-                const longitude = position.coords.longitude;
-                fetchWeatherData(latitude, longitude);
-            },
-            error => console.error('Error getting location:', error)
-        );
-    } else {
-        console.error('Geolocation is not supported by this browser.');
-    }
-}
-function randomColor(){
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for(let i=0; i<6; i++){
-        color += letters[Math.floor(Math.random()*16)];
-    }
-    return color;
-}
-
-
-setInterval(randomColor, 300);
-body.style.backgroundColor = randomColor();
 getCurrentLocation();
